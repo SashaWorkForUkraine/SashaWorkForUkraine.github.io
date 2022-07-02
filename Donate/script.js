1abc37ffa58a1d3df430bf2ad7d298da98fc78b6
IvanBanny <falco.zebrine@gmail.com>
2022-07-02 14:59:11 +0300

    feat(navbar): added "Our Work" button; feat(Donation page): changed style, added new requisite, copy button

--- Donate/script.js
+++ Donate/script.js
@@ -3,3 +3,8 @@ function toggle_navbar_list() {
     if(navbar_list[0].style.display != "none" && navbar_list[0].style.display != "") navbar_list[0].style.display = "none";
     else navbar_list[0].style.display = 'flex';
 }
+
+function copy_to_clipboard(copy_text_id) {
+    const copy_text = document.getElementById(copy_text_id);
+    navigator.clipboard.writeText(copy_text.textContent);
+}
